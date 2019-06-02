bab04a6f9dc5e0ab8c6863aa1f3132075cdd4d40
Jitendra Kumar Singh <singh.jitendra0749@yahoo.in>
2019-06-02 12:29:50 +0530

    Update check.php

--- check.php
+++ check.php
@@ -7,10 +7,13 @@
 
 <?php
 
-
+$uname=mysql_real_escape_string($_POST['uname']);
+$pass=mysql_real_escape_string($_POST['pass']);
 
 $conn=mysqli_connect('mysql.hostinger.in','u662065187_sas','@toor5','u662065187_atten');
-$query="SELECT * from faculty where uname='".$_POST['uname']."'";
+$query="SELECT * from faculty where uname='$uname' AND pass='$pass'"; 
+//you were just verifying the username not the password and it will lead to the authetication bypass 
+//as any user can just enter the username of the person and boom he got logged in without any password
 
 
 $result=mysqli_query($conn,$query)or header('Location:flogin.html');
